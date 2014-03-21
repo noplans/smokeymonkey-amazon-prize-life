ecc72f285f19f5f3260163215768e563e59cd235
Fumitake TANIGUCHI <fumitake@gmail.com>
2014-03-21 21:55:39 +0900

    Dateの表示をJSTにした * [Moment.js](http://momentjs.com/)を[cdnjs](http://cdnjs.com/)から利用

--- public/js/filters.js
+++ public/js/filters.js
@@ -1,4 +1,8 @@
 'use strict';
 
 /* Filters */
-
+angular.module('smokeymonkeyFilters', []).filter('dateConvert', function() {
+  return function(date) {
+    return moment(date).zone("+09:00").format("YYYY-MM-DD H:mm:ss");
+  };
+});
